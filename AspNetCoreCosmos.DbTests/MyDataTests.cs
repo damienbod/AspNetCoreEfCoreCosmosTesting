c034b9a19c888b9e6638e6cb4482b3055694cc23
damienbod <damien_bod@hotmail.com>
2021-06-07 20:14:23 +0200

    Adding builds

---
+++ AspNetCoreCosmos.DbTests/MyDataTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetCoreCosmos.DbTests
+{
+    public class MyDataTests : IAsyncLifetime
+    {
+
+        [Fact]
+        public Task MyDataCreate()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task InitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
